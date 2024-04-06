ef29914aed29b0bfe544f56e60932848bff8b731
swethatheresa <swethatheresa123@gmail.com>
2024-04-06 20:37:51 +0530

    added reports and login

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask, render_template, jsonify, request, Response
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 import tensorflow as tf
 import numpy as np
 from os import path, walk
 import cv2
 import base64
+from PIL import Image
+from io import BytesIO
 from collections import deque
+from dotenv import load_dotenv
+import firebase_admin
+import os
+
+from firebase_admin import credentials, storage, db
+
+UPLOAD_FOLDER = 'static/uploads'
+cred = credentials.Certificate("credentials.json")
+FIREBASE_STORAGE_BUCKET = os.getenv('FIREBASE_STORAGE_BUCKET')
+FIREBASE_DATABASE_URL = os.getenv('FIREBASE_DATABASE_URL')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 #import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
+
+firebase_admin.initialize_app(cred, {
+    'storageBucket': FIREBASE_STORAGE_BUCKET,
+    'databaseURL': FIREBASE_DATABASE_URL
+})
+
+# Firebase Storage
+bucket = storage.bucket()
+# Firebase Realtime Database
+root = db.reference()
 #config to update when template changes
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 # Load the model
@@ -21,8 +46,24 @@ FRAME_THRESHOLD = 16
 # Use deque to store frames
 frames_deque = deque(maxlen=FRAME_THRESHOLD)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Check if the username and password are correct
+        if is_valid_user(username, password):
+            session['logged_in'] = True
+            return redirect(url_for('reports'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    return render_template('login.html') 
+
 @app.route('/')
 def index():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
     return render_template('index.html')
 CLASSES_LIST = ["NonViolence", "Violence"]
 @app.route('/upload_frame', methods=['POST'])
@@ -59,7 +100,6 @@ def upload_frame():
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
 
-# ... (your existing code)
 
 def predict_video(frames_list):
     # Placeholder for your video prediction logic
@@ -82,5 +122,108 @@ def predict_video(frames_list):
 
     return predicted_class_name, confidence
 
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'frame_data' not in request.json:
+        print('No image data found')
+        return jsonify({'error': 'No image data found'})
+
+    frame_data = request.json['frame_data']
+    room_number = request.json.get('room_number')
+    time = request.json.get('time')
+
+    try:
+        image_data = base64.b64decode(frame_data)
+    except Exception as e:
+        return jsonify({'error': 'Invalid image data'})
+
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+    filename = f"{room_number}_{time}.jpg" 
+    image_path = os.path.join(UPLOAD_FOLDER, filename)
+    print(image_path)
+
+    with open(image_path, 'wb') as f:
+        f.write(image_data)
+
+
+    try:
+        bucket = storage.bucket()
+        blob = bucket.blob(filename)
+        blob.upload_from_filename(image_path)
+        blob.make_public()
+        image_url = blob.public_url
+        print(image_url)
+    except Exception as e:
+        return jsonify({'error': 'Failed to upload image to Firebase Storage'})
+
+    os.remove(image_path)
+
+    details_ref = root.child('details').push()
+    details_ref.set({
+        'image_url': image_url,
+        'room_number': room_number,
+        'time': time
+    })
+
+    return jsonify({'success': True})
+
+@app.route('/details', methods=['GET'])
+def fetch_details():
+    page = int(request.args.get('page', 1))
+    per_page = int(request.args.get('per_page', 5))
+
+    time = request.args.get('time')
+    room_number = request.args.get('room_number')
+
+    details_ref = root.child('details')
+    if time:
+        details_ref_time = details_ref.order_by_child('time').equal_to(time)
+    else:
+        details_ref_time = details_ref
+
+    if room_number:
+        details_ref_room = details_ref.order_by_child('room_number').equal_to(room_number)
+    else:
+        details_ref_room = details_ref
+
+    details = {}
+    if time and room_number:
+        for key, value in details_ref_time.get().items():
+            if key in details_ref_room.get().keys():
+                details[key] = value
+    else:
+        details = details_ref_time.get() if time else details_ref_room.get()
+
+    total_count = len(details)
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
+
+    if details:
+        details_list = list(details.values())[start_index:end_index]
+        return jsonify({
+            'total_pages': -(-total_count // per_page),
+            'data': details_list
+        })
+    else:
+        return jsonify({'error': 'No details found'}), 404
+    
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
+
+# Reports route
+@app.route('/reports')
+def reports():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
+    return render_template('reports.html')
+
+def is_valid_user(username, password):
+    valid_username = 'admin'
+    valid_password = 'admin'
+    return username == valid_username and password == valid_password
+    
 if __name__ == '__main__':
     app.run(debug=True)
